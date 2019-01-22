f61ddeaa7e6437e7e9eda545fdf190d0f23de62b
Nils <nilsnathorstwindahl@gmail.com>
2019-01-22 11:39:50 +0100

    added dependancies

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,12 @@
 const browsersync = require('rollup-plugin-browsersync');
+const babel = require('rollup-plugin-babel');
+const {
+  terser
+} = require('rollup-plugin-terser');
 const postcss = require('rollup-plugin-postcss');
+const commonjs = require('rollup-plugin-commonjs');
+const nodeResolve = require('rollup-plugin-node-resolve');
+const filesize = require('rollup-plugin-filesize');
 const normalize = require('postcss-normalize');
 const isProduction = process.env.NODE_ENV === 'production';
 const isDevelopment = isProduction === false;
@@ -8,18 +15,22 @@ module.exports = {
   input: 'src/scripts/index.js',
   output: {
     file: 'public/giphy.js',
-    format: 'iife'
+    format: 'iife',
+    sourcemap: true
   },
   plugins: [
-    (isProduction && browsersync({
-      server: 'public'
-    })),
-    // (isProduction && browsersync({proxy: "localhost:3000"})),
+    filesize(),
+    commonjs(),
+    nodeResolve(),
     postcss({
       extract: true,
       plugins: [
         normalize
       ]
-    })
+    }),
+    (isProduction && browsersync({
+      server: 'public'
+    })),
+    (isDevelopment && terser()),
   ]
 };
